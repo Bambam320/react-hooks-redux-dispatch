fb134166e98106ba7e99f02f0c51f662b5986076
capstone <igor.mestechkin@gmail.com>
2023-06-18 01:43:22 -0400

    updated

--- src/reducer.js
+++ src/reducer.js
@@ -11,3 +11,27 @@ let state = { count: 0 };
 let action = { type: "counter/increment" };
 
 changeState(state, action);
+
+function changeState(state, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+dispatch(action);
+dispatch(action);
+dispatch(action);
+dispatch(action);
+render();
